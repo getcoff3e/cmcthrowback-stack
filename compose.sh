af8b314f871ab98b74d5a9ccfd19f34e4a902e7b
coff3e <64768219+virtualcoff3e@users.noreply.github.com>
2023-03-15 10:11:20 -0500

    the balls harden

--- compose.sh
+++ compose.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./preloader.sh || chmod +x ./preloader.sh && ./preloader.sh && \
+(./preloader.sh || chmod +x ./preloader.sh && ./preloader.sh) && \
 docker-compose up -d && docker-compose status
