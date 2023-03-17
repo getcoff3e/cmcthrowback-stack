9895db31bfb6b252e79825ffbacbdeebd7841a8e
coff3e <64768219+virtualcoff3e@users.noreply.github.com>
2023-03-17 16:12:26 -0500

    Update preload-plugins.sh

--- preload-plugins.sh
+++ preload-plugins.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# you may have to copy and paste these commands by hand cause they just kind of dont work sometimes and idkwhy 
 mkdir ./plugins && \
 wget -i ./extras/mods.txt -P ./plugins/smp/
 wget -i ./extras/velocity-mods.txt -P ./plugins/velocity/
